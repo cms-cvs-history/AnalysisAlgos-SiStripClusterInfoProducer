8f3eebf884d853da1de8ccaf8abec6cb31d90fe0
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2012-04-25 13:36:08 +0000

    Initial changes for pileup memory reduction

--- python/SiStripProcessedRawDigiProducer_SimData_cfi.py
+++ python/SiStripProcessedRawDigiProducer_SimData_cfi.py
@@ -2,10 +2,10 @@ import FWCore.ParameterSet.Config as cms
 
 #defaults as in SiStripZeroSuppression_cfi.py
 siStripProcessedRawDigis = cms.EDProducer("SiStripProcessedRawDigiProducer",
-                                          DigiProducersList = cms.VInputTag( cms.InputTag('simSiStripDigis','ZeroSuppressed'),
-                                                                             cms.InputTag('simSiStripDigis','VirginRaw'), 
-                                                                             cms.InputTag('simSiStripDigis','ProcessedRaw'),
-                                                                             cms.InputTag('simSiStripDigis','ScopeMode')
+                                          DigiProducersList = cms.VInputTag( cms.InputTag('mix', 'simSiStripDigisZeroSuppressed'),
+                                                                             cms.InputTag('mix', 'simSiStripDigisVirginRaw'), 
+                                                                             cms.InputTag('mix', 'simSiStripDigisProcessedRaw'),
+                                                                             cms.InputTag('mix', 'simSiStripDigisScopeMode')
                                                                              ),
                                           CommonModeNoiseSubtractionMode = cms.string('Median'), 
                                           #CutToAvoidSignal = cms.double(3.0), ##This is just for CMNSub...Mode TT6
